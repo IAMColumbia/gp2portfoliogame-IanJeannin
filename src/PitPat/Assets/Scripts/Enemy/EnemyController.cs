41e298be035c0ef35097126565dbf65a61ed9056
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2021-03-30 14:31:58 -0400

    Prevent movement onto player. Add failure chance to AI

--- src/PitPat/Assets/Scripts/Enemy/EnemyController.cs
+++ src/PitPat/Assets/Scripts/Enemy/EnemyController.cs
@@ -13,6 +13,12 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
+    [SerializeField]
+    private float minSuccessChance=30;
+    [SerializeField]
+    private float maxSuccessChance = 100;
+    private float failureChance=30;
+
     private void Start()
     {
         if(markerHolder==null)
@@ -41,12 +47,42 @@ public class EnemyController : MonoBehaviour
     {
         if(BeatTrigger.canBePressed==true&&hasMovedThisBeat==false)
         {
+            float successChance = Random.Range(0, 100);
+            float successMarker = maxSuccessChance - failureChance;
+            if(successMarker<minSuccessChance)
+            {
+                successMarker = minSuccessChance;
+            }
+            // If the random is within the success range
+            if (successChance<= successMarker)
+            {
+                if (IsAdjacentToPlayer() == false)
+                {
+                    SetMovementVector();
+                }
+            }
             hasMovedThisBeat = true;
-            SetMovementVector();
         }
         else if(BeatTrigger.canBePressed==false&&hasMovedThisBeat==true)
         {
             hasMovedThisBeat = false;
+        }
+    }
+
+    private bool IsAdjacentToPlayer()
+    {
+        float xDistance = player.transform.position.x - this.gameObject.transform.position.x;
+        float yDistance = player.transform.position.y - this.gameObject.transform.position.y;
+        //Check if enemy is adjacent to player
+        if(xDistance<=1 && xDistance>=-1 && yDistance<=1 && yDistance>=-1&&(Mathf.Abs(xDistance)> Mathf.Abs(yDistance) || Mathf.Abs(yDistance) > Mathf.Abs(xDistance)))
+        {
+            Debug.Log("X Distance: "+xDistance);
+            Debug.Log("Y Distance: " + yDistance);
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
